287a642ebd97d08b33a4380fd32d28fd49ad9afd
Agustín Casado <agu.e.casado@gmail.com>
2022-08-28 16:31:02 -0300

    componenete de grafica de dona refactorizada con rxjs

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -15,12 +15,33 @@ export class DonaHttpComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales()
-      .subscribe( data => {
-        console.log(data);
-        const labels = Object.keys( data );
-        const values = Object.values( data );
+    //this.initDona();
+    this.initDonaRxjs();
+  }
 
+  initDona() {
+    this.graficasService.getUsuariosRedesSociales()
+    .subscribe( data => {
+      console.log(data);
+      const labels = Object.keys( data );
+      const values = Object.values( data );
+
+      this.doughnutChartData = {
+        labels: labels,
+        datasets:[
+          {
+            data:values,
+            backgroundColor: this.doughnutChartColors,
+            hoverBackgroundColor: 'blue'
+          }
+        ]
+      }
+    });
+  }
+
+  initDonaRxjs() {
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+      .subscribe( ({ labels, values }) => {
         this.doughnutChartData = {
           labels: labels,
           datasets:[

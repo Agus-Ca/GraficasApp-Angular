66309fb1cbc84e5ad76ff8f2696851506f7f574c
Agustín Casado <agu.e.casado@gmail.com>
2022-08-28 01:46:45 -0300

    menu de la app

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuItem } from '../interfaces/MenuItem.interface';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  menu: MenuItem[] = [
+    {
+      ruta: './graficas/barras',
+      texto: 'Barras'
+    },
+    {
+      ruta: './graficas/barras-dobles',
+      texto: 'Barras dobles'
+    },
+    {
+      ruta: './graficas/dona',
+      texto: 'Dona'
+    },
+    {
+      ruta: './graficas/dona-http',
+      texto: 'Dona HTTP'
+    }
+  ]
 
 }
